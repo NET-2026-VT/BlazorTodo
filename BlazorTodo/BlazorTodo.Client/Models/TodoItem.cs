f70eb0c3f3df8518c58cf234be08321a1f592dce
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-09-22 13:41:56 +0200

    Add Item Component and TodoItem.cs

---
+++ BlazorTodo/BlazorTodo.Client/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorTodo.Client.Models;
+
+public class TodoItem
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public required string Text { get; set; }
+    public DateTime Created { get; init; } = DateTime.Now;
+    public bool Completed { get; set; }
+}

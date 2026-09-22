fccc84ae1b8c40305c28bf214589e1cfe0bc739a
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-09-22 13:16:57 +0200

    NavBar with parmameter

---
+++ BlazorTodo/BlazorTodo.Client/Components/NavBar.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTodo.Client.Components;
+
+public partial class NavBar
+{
+    [Parameter]
+    public string NavTitle { get; set; } = "Default NavBar Title";
+}

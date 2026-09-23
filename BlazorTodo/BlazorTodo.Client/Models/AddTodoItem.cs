dc4fc301e1588617095da01a829544b1bf20d7fb
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-09-23 13:27:31 +0200

    Validation works

---
+++ BlazorTodo/BlazorTodo.Client/Models/AddTodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTodo.Client.Models;
+
+public class AddTodoItem
+{
+    [Required(ErrorMessage = "Please enter a todo.")]
+    [MinLength(2, ErrorMessage = "Todo must be at least 2 characters.")]
+    [MaxLength(15, ErrorMessage = "Todo can’t be longer than 15 characters.")]
+    public string Text { get; set; } = string.Empty;
+}

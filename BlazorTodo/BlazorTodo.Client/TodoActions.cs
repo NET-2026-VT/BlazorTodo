2454de862646520f1e63cad39ad1b12e20eb084a
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-09-23 11:49:03 +0200

    Ignore ToDoActions

---
+++ BlazorTodo/BlazorTodo.Client/TodoActions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTodo.Client;
+
+public class TodoActions
+{
+    public EventCallback<Guid> OnDelete { get; init; }
+    public EventCallback<Guid> OnComplette { get; init; }
+}
